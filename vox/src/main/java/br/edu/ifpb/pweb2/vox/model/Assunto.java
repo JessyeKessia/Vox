f7dccbca36c03c872322ea0670195e1c52324b28
jessyekessia <jessyekessia16@gmail.com>
2025-11-14 15:54:59 -0300

    feat: Model atualizado como parte do progresso do projeto. Agora utiliza JPA para persistência dos dados nos models

--- vox/src/main/java/br/edu/ifpb/pweb2/vox/model/Assunto.java
+++ vox/src/main/java/br/edu/ifpb/pweb2/vox/model/Assunto.java
@@ -1,23 +1,27 @@
 package br.edu.ifpb.pweb2.vox.model;
 
+import java.util.HashSet;
+import java.util.Set;
 import java.io.Serializable;
 
-// import jakarta.persistence.*;
+import jakarta.persistence.*;
 import lombok.*;
 
 
-//@Entity
-@Getter
-@Setter
+@Entity
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Assunto implements Serializable {
     private static final long serialVersionUID = 1L;
     
-    //@Id 
-    // @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+    @Id 
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
     private String nome;
+
+    @OneToMany(mappedBy = "assunto")
+     private Set<Processo> processos = new HashSet<Processo>();
 }
 
--- vox/src/main/java/br/edu/ifpb/pweb2/vox/model/Processo.java
+++ vox/src/main/java/br/edu/ifpb/pweb2/vox/model/Processo.java
@@ -1,47 +1,60 @@
 package br.edu.ifpb.pweb2.vox.model;
 
-// import jakarta.persistence.*;
+import jakarta.persistence.*;
 import lombok.*;
 
 import java.io.Serializable;
 import java.time.LocalDate;
+import java.util.UUID;
+
+import org.springframework.format.annotation.DateTimeFormat;
 
 import br.edu.ifpb.pweb2.vox.types.StatusProcesso;
 import br.edu.ifpb.pweb2.vox.types.TipoDecisao;
 
 
-
-// @Entity
-@Getter
-@Setter
+@Entity
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Processo implements Serializable {
     private static final long serialVersionUID = 1L;
-    // @Id 
-    // @GeneratedValue(strategy = GenerationType.IDENTITY)
-    // tem que trocar para long por causa do banco
-    private Integer id;
+    
+    @Id 
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-    // @ManyToOne
+    @ManyToOne
+    @JoinColumn(name = "id_assunto")
     private Assunto assunto;
 
     private String numero;
     
+    @DateTimeFormat(pattern = "dd/MM/yyyy")
     private LocalDate dataRecepcao = LocalDate.now();
 
+    @DateTimeFormat(pattern = "dd/MM/yyyy")
     private LocalDate dataDistribuicao;
 
+    @DateTimeFormat(pattern = "dd/MM/yyyy")
     private LocalDate dataParecer;
 
     private String descricao;
 
-    private StatusProcesso status;
+    @Enumerated(EnumType.STRING)
+    private StatusProcesso status = StatusProcesso.CRIADO;
 
-    // @Lob
+    @Lob
     private byte[] parecer;
 
-    // @Enumerated(EnumType.STRING)
+    @Enumerated(EnumType.STRING)
     private TipoDecisao decisaoRelator;
+
+    @PrePersist
+    public void gerarNumero() {
+        if (this.numero == null || this.numero.isEmpty()) {
+            this.numero = UUID.randomUUID().toString().substring(0, 10).toUpperCase();
+        }
+    }
 }
 
