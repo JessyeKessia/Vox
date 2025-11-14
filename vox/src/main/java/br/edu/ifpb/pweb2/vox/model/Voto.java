1c875185a17c951d30798c1c1a81005cd407e97f
jessyekessia <jessyekessia16@gmail.com>
2025-11-13 21:53:17 -0300

    feat(model): concluir modelos Voto e Reuniao

--- vox/src/main/java/br/edu/ifpb/pweb2/vox/model/Voto.java
+++ vox/src/main/java/br/edu/ifpb/pweb2/vox/model/Voto.java
@@ -1,5 +1,18 @@
 package br.edu.ifpb.pweb2.vox.model;
 
+import br.edu.ifpb.pweb2.vox.types.TipoVoto;
+// import jakarta.persistence.*;
+import lombok.*;
+
+// @Entity
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder
 public class Voto {
-    
+    // @Id
+    // @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // @Enumerated(EnumType.STRING)
+    private TipoVoto voto;
+
+    private boolean ausente;
 }
